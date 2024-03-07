eb666ac92997b21152e84fe9c28e651675cc4eac
simonbolt <150022963+simonbolt@users.noreply.github.com>
2024-03-07 19:04:45 +0800

    moved into balance fetcher and fetch balances

--- test/BalanceFetcher.fork.test.ts
+++ test/BalanceFetcher.fork.test.ts
@@ -8,7 +8,7 @@ import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import chai from "chai";
 const { expect } = chai;
 
-import { AgentFetcher } from "../typechain-types";
+import { BalanceFetcher as AgentFetcher } from "../typechain-types";
 
 import { expectDeployed } from "../scripts/utils/expectDeployed";
 import { getNetworkSettings } from "../scripts/utils/getNetworkSettings";
@@ -23,7 +23,8 @@ function convertToStruct(res: any) {
       return acc;
     }, {} as Record<string, any>);
 }
-describe("AgentFetcher", function () {
+
+describe("Balancer Fetch Forked Test", function () {
   let deployer: SignerWithAddress;
   let chainID: number;
   let networkSettings: any;
@@ -48,11 +49,11 @@ describe("AgentFetcher", function () {
     snapshot = await provider.send("evm_snapshot", []);
     await deployer.sendTransaction({ to: deployer.address }); // for some reason this helps solidity-coverage
 
-    agentFetcher = (await deployContract(
-      deployer,
-      "AgentFetcher",
-      []
-    )) as AgentFetcher;
+    agentFetcher = (await deployContract(deployer, "BalanceFetcher", [
+      deployer.address,
+      "0x4300000000000000000000000000000000000002",
+      "0xf237c20584DaCA970498917470864f4d027de4ca", // genesis
+    ])) as AgentFetcher;
     await expectDeployed(agentFetcher.address);
   });
 
@@ -60,10 +61,16 @@ describe("AgentFetcher", function () {
     await provider.send("evm_revert", [snapshot]);
   });
 
+  const erc20s = [
+    "0x0000000000000000000000000000000000000000",
+    "0x0000000000000000000000000000000000000001",
+    "0x0000000000000000000000000000000000000002",
+  ];
+
   describe("Fetch agents", function () {
     it("Can handle empty nft collection array", async function () {
       let account = "0xE89c1F56B7d46EA0Dccb8512cDE03f6Be4E94986";
-      let res = await agentFetcher.callStatic.fetchAgents(account, []);
+      let res = await agentFetcher.callStatic.fetchAgents(account, [], []);
 
       expect(res).deep.eq([]);
     });
@@ -71,7 +78,11 @@ describe("AgentFetcher", function () {
     it("Can fetch agents for eoa on one nft collection", async function () {
       let account = "0xE89c1F56B7d46EA0Dccb8512cDE03f6Be4E94986";
       let res = await agentFetcher.callStatic
-        .fetchAgents(account, ["0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b"])
+        .fetchAgents(
+          account,
+          ["0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b"],
+          erc20s
+        )
         .then((r) => r.map(convertToStruct));
 
       console.table(res);
@@ -82,84 +93,144 @@ describe("AgentFetcher", function () {
           implementation: "0x25a9aD7766D2857E4EB320a9557F637Bd748b97c",
           owner: "0xE89c1F56B7d46EA0Dccb8512cDE03f6Be4E94986",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(80),
+          tokenId: BN.from("80"),
+          balances: [
+            BN.from("0"),
+            BN.from("15119128612598"),
+            BN.from("1833481325106"),
+          ],
         },
         {
           agentAddress: "0x7ef72c9f6Cf1e50a96Aed7B1EFa4ea25B710B81C",
           implementation: "0x25a9aD7766D2857E4EB320a9557F637Bd748b97c",
           owner: "0xE89c1F56B7d46EA0Dccb8512cDE03f6Be4E94986",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(92),
+          tokenId: BN.from("92"),
+          balances: [
+            BN.from("0"),
+            BN.from("42066658252091"),
+            BN.from("4822561322368"),
+          ],
         },
         {
           agentAddress: "0x5206eB38E635ac1875afeD421dA83b8Fe8FF50Ce",
           implementation: "0x25a9aD7766D2857E4EB320a9557F637Bd748b97c",
           owner: "0xE89c1F56B7d46EA0Dccb8512cDE03f6Be4E94986",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(96),
+          tokenId: BN.from("96"),
+          balances: [
+            BN.from("0"),
+            BN.from("57088871916603"),
+            BN.from("6536652775248"),
+          ],
         },
         {
           agentAddress: "0x8Fa34e3B7c5857A011aE0482A14d18Af20a54399",
           implementation: "0x68e362fC50d62af91Aba1d9184c63505C9EA02c8",
           owner: "0xEfAce6eF46De81389DcD34849564EA5179E7A43c",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(81),
+          tokenId: BN.from("81"),
+          balances: [
+            BN.from("0"),
+            BN.from("29299232182807"),
+            BN.from("3553088039900"),
+          ],
         },
         {
           agentAddress: "0xF8568528B2eD70C9cA78694f302B41B62a65A965",
           implementation: "0x68e362fC50d62af91Aba1d9184c63505C9EA02c8",
           owner: "0x7ef72c9f6Cf1e50a96Aed7B1EFa4ea25B710B81C",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(93),
+          tokenId: BN.from("93"),
+          balances: [
+            BN.from("0"),
+            BN.from("81520589273509"),
+            BN.from("9345597134223"),
+          ],
         },
         {
           agentAddress: "0xdEEBE64D24eCb86B351Bb83Baf0D62EFA78CfA8C",
           implementation: "0x68e362fC50d62af91Aba1d9184c63505C9EA02c8",
           owner: "0x5206eB38E635ac1875afeD421dA83b8Fe8FF50Ce",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(97),
+          tokenId: BN.from("97"),
+          balances: [
+            BN.from("0"),
+            BN.from("110632258552041"),
+            BN.from("12667348917886"),
+          ],
         },
         {
           agentAddress: "0x5D2f332E759c0C0397e15110cf3d72BEB0F1Ef0e",
           implementation: "0xD9F32ab36bCB6dD3005038DeB53f9ed742947b64",
           owner: "0x8Fa34e3B7c5857A011aE0482A14d18Af20a54399",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(82),
+          tokenId: BN.from("82"),
+          balances: [
+            BN.from("0"),
+            BN.from("47081159099995"),
+            BN.from("5709484202832"),
+          ],
         },
         {
           agentAddress: "0x33EA056c4ab58d2c0736B5C6906FFBAcE782C759",
           implementation: "0xC33F80Ca19c8Cbc55837F4B6c6EC5C3FE7c4400f",
           owner: "0x8Fa34e3B7c5857A011aE0482A14d18Af20a54399",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(83),
+          tokenId: BN.from("83"),
+          balances: [
+            BN.from("0"),
+            BN.from("33805098056991"),
+            BN.from("4099509804371"),
+          ],
         },
         {
           agentAddress: "0x6179dE7Bf7816387D18a706F51767Be748629eea",
           implementation: "0xD9F32ab36bCB6dD3005038DeB53f9ed742947b64",
           owner: "0xF8568528B2eD70C9cA78694f302B41B62a65A965",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(94),
+          tokenId: BN.from("94"),
+          balances: [
+            BN.from("0"),
+            BN.from("130996172022962"),
+            BN.from("15017524539041"),
+          ],
         },
         {
           agentAddress: "0x9C0CD367Fb078b4abc092E9DbaB8E93Fee4c8bc4",
           implementation: "0xC33F80Ca19c8Cbc55837F4B6c6EC5C3FE7c4400f",
           owner: "0xF8568528B2eD70C9cA78694f302B41B62a65A965",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(95),
+          tokenId: BN.from("95"),
+          balances: [
+            BN.from("0"),
+            BN.from("94057633479799"),
+            BN.from("10782855690007"),
+          ],
         },
         {
           agentAddress: "0x06e7d4775C5b26310025902536Eb5E842fbB5B2C",
           implementation: "0xD9F32ab36bCB6dD3005038DeB53f9ed742947b64",
           owner: "0xdEEBE64D24eCb86B351Bb83Baf0D62EFA78CfA8C",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(98),
+          tokenId: BN.from("98"),
+          balances: [
+            BN.from("0"),
+            BN.from("177775392719933"),
+            BN.from("20355210659813"),
+          ],
         },
         {
           agentAddress: "0x14C0f5ef52460F9d58672077e5F7E1D2D56baEB9",
           implementation: "0xC33F80Ca19c8Cbc55837F4B6c6EC5C3FE7c4400f",
           owner: "0xdEEBE64D24eCb86B351Bb83Baf0D62EFA78CfA8C",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(99),
+          tokenId: BN.from("99"),
+          balances: [
+            BN.from("0"),
+            BN.from("127646313568934"),
+            BN.from("14615451345043"),
+          ],
         },
       ]);
     });
@@ -167,7 +238,8 @@ describe("AgentFetcher", function () {
     it("Can fetch agents for root agent", async function () {
       let res = await agentFetcher.callStatic.fetchAgents(
         "0xa9b7B191DA5749A203D8e6637C71cE4A92803F99",
-        ["0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b"]
+        ["0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b"],
+        erc20s
       );
 
       expect(res.map(convertToStruct)).deep.eq([
@@ -176,21 +248,36 @@ describe("AgentFetcher", function () {
           implementation: "0x68e362fC50d62af91Aba1d9184c63505C9EA02c8",
           owner: "0xa9b7B191DA5749A203D8e6637C71cE4A92803F99",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(21),
+          tokenId: BN.from("21"),
+          balances: [
+            BN.from("0"),
+            BN.from("157884215821716"),
+            BN.from("26537060486978"),
+          ],
         },
         {
           agentAddress: "0x5A117d079b2C1272bC2B13f57B80687D5002483f",
           implementation: "0xD9F32ab36bCB6dD3005038DeB53f9ed742947b64",
           owner: "0xC541D6cb7302535390Ff10b2AFFcf95DFD190629",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(22),
+          tokenId: BN.from("22"),
+          balances: [
+            BN.from("0"),
+            BN.from("253704472113268"),
+            BN.from("42642457241509"),
+          ],
         },
         {
           agentAddress: "0x0879DcE6101cF72545F59aE8d0b6A1A099464F8F",
           implementation: "0xC33F80Ca19c8Cbc55837F4B6c6EC5C3FE7c4400f",
           owner: "0xC541D6cb7302535390Ff10b2AFFcf95DFD190629",
           tokenAddress: "0xd1c6ABe9BEa98CA9875A4b3EEed3a62bC121963b",
-          tokenId: BN.from(23),
+          tokenId: BN.from("23"),
+          balances: [
+            BN.from("0"),
+            BN.from("182163982007902"),
+            BN.from("30617985363091"),
+          ],
         },
       ]);
     });
