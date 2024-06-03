a4f2363f293fd3d73d6a0262bc8924faf5c97379
wwHysenberg <wwhysenberg@protonmail.com>
2024-06-03 00:49:18 -0700

    test DexBalancerAgentFactory

---
+++ scripts/configuration/DexBalancerModuleA.ts
@@ -0,0 +1,32 @@
+export const moduleAFunctionParams = [
+  {
+    selector: "0x82ccd330",
+    signature: "strategyType()",
+    requiredRole:
+      "0x0000000000000000000000000000000000000000000000000000000000000000",
+  },
+  {
+    selector: "0x7bb485dc",
+    signature: "moduleA_depositBalance()",
+    requiredRole:
+      "0x0000000000000000000000000000000000000000000000000000000000000001",
+  },
+  {
+    selector: "0x740d25a3",
+    signature: "moduleA_depositBalanceAndRefundTo(address)",
+    requiredRole:
+      "0x0000000000000000000000000000000000000000000000000000000000000001",
+  },
+  {
+    selector: "0xd36bfc2e",
+    signature: "moduleA_withdrawBalance()",
+    requiredRole:
+      "0x0000000000000000000000000000000000000000000000000000000000000001",
+  },
+  {
+    selector: "0xc4fb5289",
+    signature: "moduleA_withdrawBalanceTo(address)",
+    requiredRole:
+      "0x0000000000000000000000000000000000000000000000000000000000000001",
+  }
+];
