bf850772e0af444e217ceeba6442250c68c099b2
wwHysenberg <wwhysenberg@protonmail.com>
2024-06-12 14:51:53 -0700

    fix typo

--- contracts/modules/LoopooorModuleD.sol
+++ contracts/modules/LoopooorModuleD.sol
@@ -434,7 +434,7 @@ contract LoopooorModuleD is Blastable, ILoopooorModuleD {
         moduleD_sendBalanceTo(receiver, underlying());
     }
 
-    // Send funds to reciever
+    // Send funds to receiver
     function moduleD_sendBalanceTo(address receiver, address token) public payable override {
         if (token == _eth) {
             Calls.sendValue(receiver, address(this).balance);
